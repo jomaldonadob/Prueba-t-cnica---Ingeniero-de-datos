6a8ade184bf105a8f0cc64f34607532c9e130730
jomaldonadob <jomaldonadob@unal.edu.co>
2024-11-15 03:45:01 -0500

    DockerFIle

---
+++ dockerfile
@@ -0,0 +1,33 @@
+# Usar una imagen base de Python 3.10
+FROM python:3.10-slim
+
+# Instalar dependencias del sistema
+RUN apt-get update && apt-get install -y \
+    python3-tk \
+    openjdk-17-jre-headless \
+    procps \
+    && rm -rf /var/lib/apt/lists/*
+
+# Establecer JAVA_HOME
+ENV JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+ENV PATH=$JAVA_HOME/bin:$PATH
+
+# Establecer el directorio de trabajo
+WORKDIR /app
+
+# Copiar los archivos de requisitos y el código al contenedor
+COPY requirements.txt requirements.txt
+COPY . .
+
+# Copiar el archivo de datos al contenedor
+COPY data/Films_2.xlsx /app/data/Films_2.xlsx
+
+# Crear y activar un entorno virtual
+RUN python -m venv venv
+ENV PATH="/app/venv/bin:$PATH"
+
+# Instalar las dependencias de Python en el entorno virtual
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Comando para ejecutar el script principal
+CMD ["python", "main.py"]
--- etl/data_cleaner.py
+++ etl/data_cleaner.py
@@ -2,18 +2,18 @@ from pyspark.sql import functions as F
 from pyspark.sql.types import DecimalType, IntegerType
 
 def clean_film_data(df_film):
-    # Limpieza de `film_id`: extraer solo los números, y asegurar que es un entero
+    #Limpieza de `film_id`: extraer solo los números, y asegurar que es un entero
     df_film = df_film.withColumn("film_id", 
-        F.regexp_extract("film_id", r"(\d+)", 1).cast(IntegerType()))  # Extrae solo el primer grupo de números encontrados
+        F.regexp_extract("film_id", r"(\d+)", 1).cast(IntegerType()))
 
     df_film = df_film.withColumn("rental_rate", 
         F.regexp_extract("rental_rate", r"(\d+\.\d{2})", 1).cast(DecimalType(4, 2)))
 
-    # Limpieza de `release_year`: extraer solo los números de 4 dígitos para el año
+    #Limpieza de `release_year`: extraer solo los números de 4 dígitos para el año
     df_film = df_film.withColumn("release_year", 
-        F.regexp_extract("release_year", r"(\d{4})", 1).cast(IntegerType()))  # Extrae solo el primer grupo de 4 dígitos
+        F.regexp_extract("release_year", r"(\d{4})", 1).cast(IntegerType()))  
     
-    # Limpieza de `length`: extraer solo los números y convertir a SMALLINT
+    #Limpieza de `length`: extraer solo los números y convertir a SMALLINT
     df_film = df_film.withColumn("length", 
         F.regexp_extract("length", r"(\d+)", 1).cast(IntegerType()))
     
@@ -23,33 +23,33 @@ def clean_film_data(df_film):
     df_film = df_film.withColumn("num_voted_users", 
         F.regexp_extract("num_voted_users", r"(\d+)", 1).cast(IntegerType()))
     
-    # Limpieza de `rating`: Asegurar que el valor esté en una lista de valores válidos
+    #Limpieza de `rating`: Asegurar que el valor esté en una lista de valores válidos
     valid_ratings = ["G", "PG", "PG-13", "R", "NC-17"]
     df_film = df_film.withColumn("rating",
         F.when(df_film["rating"].isin(valid_ratings), df_film["rating"])
-         .otherwise("NR"))  # Asigna un valor por defecto ("NR") para valores inválidos en lugar de `None`
+         .otherwise("NR"))  
     
-    # Guardar el DataFrame limpio
+    #Guardar el DataFrame limpio
     df_film.write.mode("overwrite").parquet("output_data/film_cleaned.parquet")
     print("Datos limpiados y guardados en 'output_data/film_cleaned.parquet'")
 
 def clean_inventory_data(df_inventory):
-    # Limpieza de `store_id`: extraer solo los números y convertir a TINYINT
+    #Limpieza de `store_id`: extraer solo los números y convertir a TINYINT
     df_inventory = df_inventory.withColumn("store_id", 
         F.regexp_extract("store_id", r"(\d+)", 1).cast(IntegerType()))
-    # Guardar el DataFrame limpio
+    #Guardar el DataFrame limpio
     df_inventory.write.mode("overwrite").parquet("output_data/inventory_cleaned.parquet")
 
     print("Limpieza de datos de inventory completada y guardada en 'output_data/inventory_cleaned.parquet'.")
 
 
 def clean_customer_data(df_customer):
-    # Limpiar `firstname` y `lastname`
+    #Limpiar `firstname` y `lastname`
     df_customer_cleaned = df_customer \
         .withColumn("first_name", F.upper(F.regexp_replace("first_name", r"[^A-Z]", ""))) \
         .withColumn("last_name", F.upper(F.regexp_replace("last_name", r"[^A-Z]", ""))) \
-        .withColumn("email", F.regexp_replace("email", r"\\r$", "")) # Limpiar `email` eliminando el carácter "\r" al final si está presente
-    # Guardar el DataFrame limpio
+        .withColumn("email", F.regexp_replace("email", r"\\r$", ""))
+    #Guardar el DataFrame limpio
     df_customer_cleaned.write.mode("overwrite").parquet("output_data/customer_cleaned.parquet")
     
     print("Limpieza de datos en customer completada.")
